8b037d3ee7e2681b985a270b38f0cacc3abdea14
Manpreet-Tiwari <105644197+Manpreet-Tiwari@users.noreply.github.com>
2022-06-24 10:35:14 +0530

    Second change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { TodoComponent } from './MyComponent/todo/todo.component';
 import { FormsModule } from '@angular/forms';
-import { ServersComponent } from './servers/servers.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoComponent,
-    ServersComponent
+    TodoComponent
+ 
   ],
   imports: [
     BrowserModule,
